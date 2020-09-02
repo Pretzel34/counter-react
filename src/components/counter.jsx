1a2ecf21f9ecc8dd6c854b22c191a00cec5cb914
Preston <Preston@DESKTOP-PP6FINU>
2020-09-02 14:58:01 -0600

    New examples for parent inheritence

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,59 +1,70 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: [],
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  //   tags: [],
+  // };
 
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this.state.count);
   // }
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul className="li">
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  // renderTags() {
+  //   if (this.state.tags.length === 0);
+  //   return (
+  //     <ul className="li">
+  //       {this.state.tags.map((tag) => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
 
-  handleIncrement = (product) => {
-    this.setState({ count: this.state.count + product });
-    console.log("Increment CLicked", this.state.count);
-  };
+  // handleIncrement = (product) => {
+  //   this.setState({ value: this.state.value + product });
+  //   console.log("Increment CLicked", this.state.value);
+  // };
 
   render() {
-    let classes = this.getBadgeNames();
-
     return (
-      <div>
-        <span className={classes}>{this.formatCount()}</span>
-        <button
-          onClick={() => this.handleIncrement(1)}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-        <div>
-          {this.state.tags.length === 0 && "Please create new tag!"}
-          {this.renderTags()}
+      <div className="row">
+        <div className="col-1">
+          <span className={this.getBadgeNames()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button
+            onClick={() => this.props.onIncrement(this.props.counter)}
+            className="btn btn-secondary btn-sm m-2"
+          >
+            +
+          </button>
+          <button
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0}
+          >
+            -
+          </button>
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm m-2"
+          >
+            X
+          </button>
         </div>
       </div>
     );
   }
   getBadgeNames() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
